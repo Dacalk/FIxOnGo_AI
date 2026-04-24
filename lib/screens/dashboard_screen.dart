31ba46655b37e3e3dc40954ca851935832b6d2f9
Anuradha Lakshitha <126761493+Anuradha-LG@users.noreply.github.com>
2026-04-24 16:10:38 +0530

    Revert "update job role section"

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -880,7 +880,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     title: 'Job History',
                     color: const Color(0xFF1565C0),
                     onTap: () {
-                      Navigator.pushNamed(context, '/job-history');
+                      Navigator.pushNamed(context, '/ai-chat-history');
                     },
                   ),
                 ),
